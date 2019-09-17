674996c85bec2ddf6581f65a8e94ffd2f9cc108a
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-09-16 21:09:17 -0500

    Set server to true 🛠

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -24,6 +24,7 @@ secret_key_base =
     """
 
 config :creek_dict_admin, CreekDictAdminWeb.Endpoint,
+  server: true,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
 
