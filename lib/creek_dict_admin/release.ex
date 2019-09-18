b1b46d3caa7297784c1b7d9bd1315c28c89b5c45
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-09-17 22:04:44 -0500

    Update ecto config 🛠

--- lib/creek_dict_admin/release.ex
+++ lib/creek_dict_admin/release.ex
@@ -2,12 +2,16 @@ defmodule CreekDictAdmin.Release do
   @app :creek_dict_admin
 
   def migrate do
+    Application.ensure_all_started(@app)
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    Application.ensure_all_started(@app)
+
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
